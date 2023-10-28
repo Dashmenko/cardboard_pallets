04e55f7fba8b20d0d2ea0c8c52b532e434677d0d
Darya_Yakimenka <dashmenko@gmail.com>
2023-10-29 01:21:49 +0300

    init

---
+++ js/app.js
@@ -0,0 +1,13 @@
+// copy 
+
+document.getElementById("copy_viber").addEventListener('click', function () {
+    navigator.clipboard.writeText("+375339040557");
+});
+
+document.getElementById("copy_whatsapp").addEventListener('click', function () {
+    navigator.clipboard.writeText("+375339040557");
+});
+
+document.getElementById("copy_mail").addEventListener('click', function () {
+    navigator.clipboard.writeText("mail@gmail.com");
+});
